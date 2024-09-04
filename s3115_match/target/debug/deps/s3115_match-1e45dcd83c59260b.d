8d43cfca889ff18ace385dead519d4e22fea9127
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-09-04 09:32:44 -0300

    s3115 04/09/2024

---
+++ s3115_match/target/debug/deps/s3115_match-1e45dcd83c59260b.d
@@ -0,0 +1,5 @@
+C:\rust1\s3115_match\target\debug\deps\libs3115_match-1e45dcd83c59260b.rmeta: src/main.rs
+
+C:\rust1\s3115_match\target\debug\deps\s3115_match-1e45dcd83c59260b.d: src/main.rs
+
+src/main.rs:
---
+++ s3115_match/target/debug/deps/s3115_match-3bf3724675eb94f5.d
@@ -0,0 +1,5 @@
+C:\rust1\s3115_match\target\debug\deps\libs3115_match-3bf3724675eb94f5.rmeta: src/main.rs
+
+C:\rust1\s3115_match\target\debug\deps\s3115_match-3bf3724675eb94f5.d: src/main.rs
+
+src/main.rs:
