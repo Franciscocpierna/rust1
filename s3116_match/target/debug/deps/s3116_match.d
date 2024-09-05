17a8c1e195d399bb1bc5f21ebe976c1616e1d3dd
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-09-05 09:38:38 -0300

    s3116 05/09/2024

---
+++ s3116_match/target/debug/deps/s3116_match.d
@@ -0,0 +1,5 @@
+C:\rust1\s3116_match\target\debug\deps\s3116_match.exe: src/main.rs
+
+C:\rust1\s3116_match\target\debug\deps\s3116_match.d: src/main.rs
+
+src/main.rs:
---
+++ s3116_match/target/debug/s3116_match.d
@@ -0,0 +1,1 @@
+C:\rust1\s3116_match\target\debug\s3116_match.exe: C:\rust1\s3116_match\src\main.rs
