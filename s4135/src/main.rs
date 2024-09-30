6a023e8c522eae9680a524933b4126dd248f222a
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-09-30 18:53:12 -0300

    s4135  30/09/2024

---
+++ s4135/src/main.rs
@@ -0,0 +1,16 @@
+
+fn media(numeros: Vec<i32>) -> f64{
+    0.
+}
+ 
+fn mediana(numeros: Vec<i32>) -> f64{
+    0.
+}
+ 
+fn moda(numeros: Vec<i32>) -> i32{
+    0
+}
+
+fn main() {
+    println!("Hello, world!");
+}
