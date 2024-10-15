3f77b50b895cfb2d1b7dd01deb79133efa6b70ac
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-10-15 15:16:34 -0300

    s4158_trait 15/10/2024

---
+++ s4158_trait/src/main.rs
@@ -0,0 +1,17 @@
+mod it;
+use it::{Router, Network};
+
+fn main() {
+    let router = Router {
+        ip: "192.168.1.1".to_string(),
+    };
+    
+    let is_alive = router.ping("google.com");
+    let trace = router.traceroute("google.com");
+    let ip = router.nslookup("google.com");
+
+    println!("O host está online: {}", is_alive);
+    println!("Trace de rota: {:?}", trace);
+    println!("Endereço IP: {}", ip);
+}
+
