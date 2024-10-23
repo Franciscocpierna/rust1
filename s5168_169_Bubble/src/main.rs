3ff0fab06ac8ef42e85f3a73fe49fe187572cd1d
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-10-23 09:20:58 -0300

    s5168_169 23/10/2024

---
+++ s5168_169_Bubble/src/main.rs
@@ -0,0 +1,24 @@
+//ordenação Bubble 
+fn swap_array(lista:&mut [i32;7],i: usize, j: usize){
+    let temp = lista[i];
+    lista[i]=lista[j];
+    lista[j]=temp;
+}
+
+
+fn main() {
+    let mut array: [i32;7] = [10, 23, 4, 5, 66, 7, -3];
+    println!("{:?}", array);
+    for i in 0..array.len(){
+         for j in ((i+1)..array.len()).rev(){
+             println!("i = {} j = {}",i,j);
+             println!("j-1 = {} j = {}",array[j-1],array[j]);
+             if array[j-1] > array[j]{
+                swap_array(&mut array, j-1, j);     
+
+             }
+             println!("{:?}", array);
+} 
+         }
+    }
+    
