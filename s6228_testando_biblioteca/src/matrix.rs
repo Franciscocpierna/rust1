bb997aea65b967ba76054356ce5e3e89d6d78301
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-12-07 13:50:44 -0300

    231. Criando diretório de tests para as Bibliotecas Parte 2

--- s6228_testando_biblioteca/src/matrix.rs
+++ s6228_testando_biblioteca/src/matrix.rs
@@ -10,6 +10,7 @@ impl Matrix{
     pub fn new(rows: usize, cols: usize, data: Vec<Vec<f64>>) -> Self{
         Matrix{rows, cols, data}
     }
+
     pub fn add(&self, other: &Matrix) ->Result<Matrix, &'static str>{
         if self.rows  != other.rows || self.cols != other.cols{
             return Err("As dimensões das matrizes não são compativeis");
@@ -24,5 +25,34 @@ impl Matrix{
             result_data.push(row);
         }
         Ok(Matrix::new(self.rows, self.cols, result_data))
+    }
+
+    pub fn multipy(&self, other: &Matrix) -> Result<Matrix, &'static str>{
+        // 3 x 2  2 x 5 => 3x5 para multiplicação de matrizes
+         if self.cols != other.rows{
+           return Err("as dimensões das marizes não são compativeis para multiplicação");
+         }
+         let mut result_data = Vec::with_capacity(self.rows);
+         for i in 0..self.rows{
+            let mut rows = Vec::with_capacity(self.cols);
+            for j in 0..other.cols{
+               let mut sum = 0.0;
+               for k in 0..self.cols{
+                 sum += self.data[i][k] * other.data[k][j];
+               }
+               rows.push(sum);
+            }
+            result_data.push(rows);    
+        }
+       
+        Ok(Matrix::new(self.rows, other.cols, result_data))   
+    }
+    pub fn print(&self){
+        for row in &self.data{
+            for &element in row{
+                print!("{}", element);
+            }
+            println!();
+        }
     }
 }
--- s6228_testando_biblioteca/tests/matrix_test.rs
+++ s6228_testando_biblioteca/tests/matrix_test.rs
@@ -9,6 +9,23 @@ fn test_add_matrices(){
         result.data,
         vec![vec![8.0, 10.0, 12.0], vec![14.0, 16.0,18.0]]
     );
+}
+
+#[test]
+fn test_multiply_matrices(){
+    let matrix_a = Matrix::new(2, 3, vec![vec![1.0, 2.0, 3.0], vec![4.0, 5.0, 6.0]]);
+    let matrix_b = Matrix::new(3, 2, vec![vec![7.0, 8.0], vec![9.0, 10.0], vec![11.0, 12.0]]);
+
+    //c11, c12, c21, c22 => c11= 1*7 + 2*9 + 3*11 = 58
+   //                      c12= 1*8 + 2*10 + 3*12 = 64
+   //                      c21= 4*7 + 5*9 + 6*11 = 139
+   //                      c22= 4*8 + 5*10 + 6*12 = 154                
+   // 
+   let result = matrix_a.multipy(&matrix_b).unwrap();
+   assert_eq!(
+       result.data,
+       vec![vec![58.0, 64.0], vec![139.0, 154.0]]
+   );
 
 
 }
