618aed81d07e4603ca5daa5c6eebae917e19f33e
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-02-11 14:29:29 -0300

    todo_app 11/02/2025

--- todo_app/src/repository.rs
+++ todo_app/src/repository.rs
@@ -10,5 +10,20 @@ pub struct TodoRepo{
 
 }
 impl TodoRepo{
-    
+    pub fn get(&self,id: Uuid) -> Result<Todo, TodoRepoError>{
+        seif.items.get(&id).cloned().ok_or(TodoRepoError::NotFound)
+    }
+    pub fn list(&self, filter: &TodoListFilter) -> Vec<Todo>{
+       let mut todos = self.
+          items.values().
+          filter(|item| match filter{
+                TodoListFilter::Completed => item.is_completed,
+                TodoListFilter::Active => !item.is_completed,
+                TodoListFilter::All => true,
+          })
+          .cloned()
+          .collect::<Vec<_>>();
+        todos.sort_by(|a, b| b.created_at.cmp(&a.created_at));
+        todos
+    }
 }
