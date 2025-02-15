e57600c0c5cc6c815c0d885c6a3796a661bcc52f
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-02-15 08:31:30 -0300

    desenvolvimento_web_rust 15/02/2025

--- desenvolvimento_web_rust/src/main.rs
+++ desenvolvimento_web_rust/src/main.rs
@@ -8,15 +8,36 @@ extern crate rocket;
 fn index() -> &'static str {
     "Hello, Rocket!"
 }
+#[get("/hello/<name>")]
+fn hello(name: String) -> String {
+    format!("Hello, {}!", name)
+}
+ #[get("/number/<number>")]
+ fn number(number: i32) -> String {
+    format!("Number: {}", number)
+ }
+
+/* #[get("/search?<query>&<max_results>&<page>")] // exemplo funcionou
+ fn search(query: String,max_results: i32,page: i32) -> String {
+    format!("Searching for: {}, max_results: {}, page: {}", query, max_results, page)
+ }
+*/
+
+#[get("/search?<query>&<typ>")]
+fn search(query: String, typ: Option<String>) -> String {
+    match typ {
+        Some(t) => format!("Searching for: {}, (type: {})", query, t),
+        None => format!("Searching for: {} (no type specified)", query)
+    }
+ }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index,hello,number,search])
 }
 
 /*fn main() {
-    println!("Hello, world!");
+    rocket().launch();
     
-}
-*/
+}*/
 
